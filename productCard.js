8700cacfcfd761fd21ee54c01e31f71dc267264e
Joriyx <jorgineoseland@gmail.com>
2026-02-19 21:52:31 +0100

    Update productCard.js
    added code for making product card

--- productCard.js
+++ productCard.js
@@ -1,1 +1,40 @@
-function createProductCard(product) {}
+function createProductCard(product) {
+  const article = document.createElement("article");
+  article.className = "product_card";
+
+  const favoriteIcon = document.createElement("a");
+  favoriteIcon.className = "heart";
+
+  article.appendChild(favoriteIcon);
+
+  const icon = document.createElement("img");
+  icon.src = "";
+  icon.alt = "line art of a heart";
+  icon.className = "icons";
+  favoriteIcon.appendChild(icon);
+
+  const productLink = document.createElement("a");
+  productLink.className = "product_card_photo";
+  article.appendChild(productLink);
+
+  const productImage = document.createElement("img");
+  productImage.src = product.image.url;
+  productImage.alt = product.image.alt;
+  productLink.appendChild(productImage);
+
+  const h3 = document.createElement("h3");
+  h3.textContent = product.title;
+  h3.className = "product_card_name";
+  article.appendChild(h3);
+
+  const price = document.createElement("p");
+  price.textContent = product.price;
+  price.className = "product_card_price";
+  article.appendChild(price);
+
+  const button = document.createElement("a");
+  button.href = "";
+  button.className = "product_card_cta";
+  button.textContent = "Shop Now";
+  article.appendChild(button);
+}
